3383e65afeab313c2051b71403c4af89a498734c
noalbertin <jeanneaurelle100@gmail.com>
2025-04-14 09:54:06 +0300

    Mise à jour de mon projet

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,7 +1,11 @@
+// app/head.tsx
 export default function Head() {
     return (
       <>
         <meta name="google-site-verification" content="kLIWa6XfD_tBSAVIdEg902-TgyK4XT3gI-yliuPBErk" />
+        <title>Portfolio Sylvano Albertin</title>
+        <meta name="description" content="Nirindrainy Sylvano Albertin developpeur" />
+        <link rel="icon" href="/favicon.png" />
       </>
     );
   }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,11 +20,9 @@ export const metadata: Metadata = {
   description: "Nirindrainy Sylvano Albertin developpeur",
   icons: {
     icon: "/favicon.png",
-  },
-  other: {
-    'google-site-verification': 'kLIWa6XfD_tBSAVIdEg902-TgyK4XT3gI-yliuPBErk',
   }
 };
+
 
 
 export default function RootLayout({
