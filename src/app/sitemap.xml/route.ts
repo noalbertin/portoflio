c95507cdb55a0f0459a8d3528d6aa6cbe473d336
noalbertin <jeanneaurelle100@gmail.com>
2025-04-14 10:41:02 +0300

    Ajout fichier de vérification Google Search Console

--- src/app/sitemap.xml/route.ts
+++ src/app/sitemap.xml/route.ts
@@ -20,10 +20,13 @@ export async function GET() {
   </url>`
   ).join('');
 
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${urls}
-  </urlset>`;
+  const sitemap = [
+    '<?xml version="1.0" encoding="UTF-8"?>',
+    '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
+    urls,
+    '</urlset>',
+  ].join('');
+  
 
   return new NextResponse(sitemap, {
     headers: {
